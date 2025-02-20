f19db4b7f25fae41c8a80481f2f9fd156711ce30
aigoncharov <andrey@goncharov.page>
2025-02-20 15:07:22 +0300

    Add new GPQA estimates

--- utils/prompt.py
+++ utils/prompt.py
@@ -18,3 +18,10 @@ def get_user_prompt(question: str, options: List[str]):
     options_str = "\n".join([f"{option_id}. {answer}".strip() for option_id, answer in zip(option_ids, options)])
     user_prompt = f"Question: {question.strip()}\nOptions:\n{options_str}\nChoose one of the answers. Write down ONLY the NUMBER of the correct answer and nothing else."
     return user_prompt
+
+
+estimate_numerical_complexity_system_prompt = 'You are an expert in the topic of the question. Please act as an impartial judge and evaluate the complexity of the multiple-choice question with options below. Begin your evaluation by providing a short explanation. Be as objective as possible. After providing your explanation, you must not answer the question. You must rate the question complexity as a number from 0 to 1 following the following scale as a reference: high_school_and_easier - 0.0-0.25, undergraduate - 0.25-0.5, graduate - 0.5-0.75, postgraduate - 0.75-1.0. You must return the complexity by strictly following this format: "[[complexity]]", for example: "Complexity: [[0.48]]", which corresponds to a hard question at the undergraduate level.'
+
+
+valid_nominal_complexities = ["high_school_and_easier", "undergraduate", "graduate", "postgraduate"]
+estimate_nominal_complexity_system_prompt = f'You are an expert in the topic of the question. Please act as an impartial judge and evaluate the complexity of the multiple-choice question with options below. Begin your evaluation by providing a short explanation. Be as objective as possible. After providing your explanation, you must not answer the question. You must rate the question complexity by strictly following the scale: {", ".join(valid_nominal_complexities)}. You must return the complexity by strictly following this format: "[[complexity]]", for example: "Complexity: [[undergraduate]]", which corresponds to the undergraduate level.'
